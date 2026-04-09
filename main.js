38ac9e23236089fbbdd1b4e14e7ec6d338c83643
vldeycoid <restubumi0901201@gmail.com>
2026-04-09 19:20:23 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ shareBtn.onclick = () => {
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://whatsapp.com/channel/0029VbCCA6g1NCrOEI15qK07");
+    window.open("https://whatsapp.com/channel/0029VbCKzuEFXUugctLPPy1D");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/17qXKSuWiV/");
+    window.open("https://www.facebook.com/profile.php?id=100023500530215");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -34,6 +34,9 @@ overlay.addEventListener("click", () => {
     // Aksi setelah klik
     window.open("https://s.shopee.co.id/8zyp8hZrSG", "_blank");
 });
+
+
+
 
 
 
